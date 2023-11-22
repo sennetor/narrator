77507bd1c592d2226d2336356b20a8566c1672f7
sennetor <104866861+sennetor@users.noreply.github.com>
2023-11-22 21:12:32 +0000

    Create stream.py
    I've modified the play audio function to split the text up into chunks using the Re library and stream the results. You will need to install mpv player https://mpv.io/
    
    I've also added the option to select the faster voice model from 11 labs - this is a lot faster.

---
+++ stream.py
@@ -0,0 +1,107 @@
+import os
+import re
+from openai import OpenAI
+import base64
+import json
+import time
+import simpleaudio as sa
+import errno
+from elevenlabs import generate, play, set_api_key, voices, stream
+
+client = OpenAI()
+
+set_api_key(os.environ.get("ELEVENLABS_API_KEY"))
+
+def encode_image(image_path):
+    while True:
+        try:
+            with open(image_path, "rb") as image_file:
+                return base64.b64encode(image_file.read()).decode("utf-8")
+        except IOError as e:
+            if e.errno != errno.EACCES:
+                # Not a "file in use" error, re-raise
+                raise
+            # File is being written to, wait a bit and retry
+            time.sleep(0.1)
+
+def text_stream(text):
+    # Split the text into sentences or phrases
+    sentences = re.split(r'(?<=[.!?]) +', text)
+    for sentence in sentences:
+        yield sentence
+
+def play_audio(text):
+    # Convert text to a generator of sentences
+    audio_stream = generate(
+        text=text_stream(text),
+        model="eleven_turbo_v2",
+        voice=os.environ.get("ELEVENLABS_VOICE_ID"),
+        stream=True
+    )
+
+    # Stream the audio
+    stream(audio_stream)
+
+
+def generate_new_line(base64_image):
+    return [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "Describe this image"},
+                {
+                    "type": "image_url",
+                    "image_url": f"data:image/jpeg;base64,{base64_image}",
+                },
+            ],
+        },
+    ]
+
+
+def analyze_image(base64_image, script):
+    response = client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.
+                Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!
+                """,
+            },
+        ]
+        + script
+        + generate_new_line(base64_image),
+        max_tokens=500,
+    )
+    response_text = response.choices[0].message.content
+    return response_text
+
+
+def main():
+    script = []
+
+    while True:
+        # path to your image
+        image_path = os.path.join(os.getcwd(), "./frames/frame.jpg")
+
+        # getting the base64 encoding
+        base64_image = encode_image(image_path)
+
+        # analyze posture
+        print("👀 David is watching...")
+        analysis = analyze_image(base64_image, script=script)
+
+        print("🎙️ David says:")
+        print(analysis)
+
+        play_audio(analysis)
+
+        script = script + [{"role": "assistant", "content": analysis}]
+
+        # wait for 5 seconds
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
